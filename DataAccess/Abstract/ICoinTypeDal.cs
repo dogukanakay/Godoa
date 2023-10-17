51f56d24165b6723f6e0279f6c8fdf2a0ca2909e
dogukanakay <akay.dogukann@gmail.com>
2023-10-17 18:42:39 +0300

    ConreteDal

--- DataAccess/Abstract/ICoinTypeDal.cs
+++ DataAccess/Abstract/ICoinTypeDal.cs
@@ -1,19 +1,11 @@
 ﻿using Core.DataAccess;
 using Entities.Concrete;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Core.DataAccess;
-using Entities.Concrete;
 
 namespace DataAccess.Abstract
 {
-    public interface ICoinTypeDal:IEntityRepository<CoinType>
+    public interface ICoinTypeDal : IEntityRepository<CoinType>
     {
 
     }
 
-   
 }
---
+++ DataAccess/Concrete/EntityFramework/Context/GodoaContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework.Context
+{
+    public class GodoaContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=ABRA; Database= Godoa; Trusted_Connection=true");
+        }
+    }
+}
