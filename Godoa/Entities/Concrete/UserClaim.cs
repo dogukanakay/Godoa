fd39e6d73305f62982a112d9a117985174654822
dogukanakay <akay.dogukann@gmail.com>
2023-10-17 16:51:35 +0300

    Add project files.

---
+++ Godoa/Entities/Concrete/UserClaim.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities.Concrete
+{
+    public class UserClaim:IEntity
+    {
+        [Key]
+        public int UserClaimId { get; set; }
+        public int UserId { get; set; }
+        public int ClaimId { get; set; }
+    }
+}
