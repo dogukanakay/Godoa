9562ec77172210e3c83fcbb704ea382ccd4df605
dogukanakay <akay.dogukann@gmail.com>
2023-10-17 19:05:17 +0300

    Dal is OVER

--- DataAccess/Concrete/EntityFramework/Context/GodoaContext.cs
+++ DataAccess/Concrete/EntityFramework/Context/GodoaContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Core.Entities.Concrete;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +11,24 @@ namespace DataAccess.Concrete.EntityFramework.Context
 {
     public class GodoaContext : DbContext
     {
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<CoinType> CoinTypes { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Game> Games { get; set; }
+        public DbSet<GameKey> GameKeys { get; set; }
+        public DbSet<GameType> GameTypes { get; set; }
+        public DbSet<InGameCoin> inGameCoins { get; set; }
+        public DbSet<Keys> Keys { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+        public DbSet<Platform> Platforms { get; set; }
+        public DbSet<Seller> Sellers { get; set; }
+        public DbSet<Skin> Skins { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Claim> Claims { get; set; }
+        public DbSet<UserClaim> UserClaims { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(@"Server=ABRA; Database= Godoa; Trusted_Connection=true");
--- DataAccess/Concrete/EntityFramework/EfSkinDal.cs
+++ DataAccess/Concrete/EntityFramework/EfSkinDal.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfProductDal : EfEntityRepositoryBase<Product,GodoaContext> , IProductDal
+    public class EfSkinDal : EfEntityRepositoryBase<Skin,GodoaContext>, ISkinDal
     {
     }
 }
