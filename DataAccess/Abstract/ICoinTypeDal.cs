63c5d770a05b836209a39429bb81d2c4c7469f76
Gökdeniz Genç <fbgokdeniz@gmail.com>
2023-10-17 18:29:06 +0300

    fix

--- DataAccess/Abstract/ICoinTypeDal.cs
+++ DataAccess/Abstract/ICoinTypeDal.cs
@@ -10,11 +10,10 @@ using Entities.Concrete;
 
 namespace DataAccess.Abstract
 {
-<<<<<<<< HEAD:DataAccess/Abstract/ICoinTypeDal.cs
     public interface ICoinTypeDal:IEntityRepository<CoinType>
-========
-    public interface IKeyDal : IEntityRepository<Keys>
->>>>>>>> 164d5ece7539f4648d6aa67bfd5560a563c10309:DataAccess/Abstract/IKeyDal.cs
     {
+
     }
+
+   
 }
