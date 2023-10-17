3b9c039021db56ace98f04cc5e757ddea55d7ebe
dogukanakay <akay.dogukann@gmail.com>
2023-10-17 17:27:20 +0300

    Entities Added

---
+++ Entities/Concrete/Payment.cs
@@ -0,0 +1,21 @@
+﻿using Core.Entities.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class Payment : IEntity
+    {
+        [Key]
+        public int PaymentId { get; set; }
+        public int OrderId { get; set; }
+        public int UserId { get; set; }
+        public DateTime PaymentDate { get; set; }
+        public bool Status { get; set; }
+
+    }
+}
