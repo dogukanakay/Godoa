4d4105378586f4a1189a9d813c82f9a33294d9c0
dogukanakay <akay.dogukann@gmail.com>
2023-10-18 20:37:40 +0300

    HALLA HALLA HALLLA BU NASIL BİR KOD

---
+++ WebApi/Controllers/PlatformController.cs
@@ -0,0 +1,33 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlatformController : ControllerBase
+    {
+        IPlatformService _platformService;
+
+
+        public PlatformController(IPlatformService platformService)
+        {
+            _platformService = platformService;
+        }
+
+        [HttpGet("getall")]
+
+        public IActionResult GetAll()
+        {
+            var result = _platformService.GetAll();
+
+            if(result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+
+        }
+    }
+}
