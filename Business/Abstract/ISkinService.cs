d62b19e91d7bfc6825f66e6d2353f17239d67f88
dogukanakay <akay.dogukann@gmail.com>
2023-10-18 17:55:15 +0300

    Result Yapısı Kuruldu

---
+++ Business/Abstract/ISkinService.cs
@@ -0,0 +1,20 @@
+﻿using Core.Utilities.Results;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface ISkinService
+    {
+        IResult Add(Skin skin);
+        IResult Delete(Skin skin);
+        IResult Update(Skin skin);
+
+        IDataResult<List<Skin>> GetAll();
+        IDataResult<Skin> Get();
+    }
+}
