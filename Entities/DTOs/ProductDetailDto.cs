18aa0726e96be446d85570a4962729f6eb80736b
dogukanakay <akay.dogukann@gmail.com>
2023-12-20 22:29:37 +0300

    Product

--- Entities/DTOs/ProductDetailDto.cs
+++ Entities/DTOs/ProductDetailDto.cs
@@ -11,11 +11,6 @@ namespace Entities.DTOs
     {
         public int ProductId { get; set; }
         public string ProductCategoryName { get; set; }
-<<<<<<< HEAD
-        public int ProductCategoryId { get; set; }
-=======
-        public string GameName { get; set; }
->>>>>>> db4722c4c4a36d55cae5aaa0371e864f4a1da301
         public string ProductName { get; set; }
         public string Description { get; set; }
         public double Price { get; set; }
